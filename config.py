7d36f3720b4ddb802630a00fc2a6d4b4fb181a6c
Ankit Bindal <ankitb.1997@gmail.com>
2018-04-16 02:07:40 +0530

    Added configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+IMG_SIZE = (100, 100, 3)
+CROPPED_SIZE = (?)
+
+driving_log_fname = "logs.csv"
+train_tfrecord_fname = "train_dataset.tfrecords"
+valid_tfrecord_fname = "valid_dataset.tfrecords"
+test_tfrecord_fname = "test_dataset.tfrecords"
+meta_dataset = '.dataset-metadata'
+
+batch_size = 8
